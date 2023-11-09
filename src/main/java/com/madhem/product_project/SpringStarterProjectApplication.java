f29dadb5f9539e85019f72c0450a626ca8560349
Redcroow <kilian.mvrl@gmail.com>
2023-11-09 11:21:59 +0100

    Initial commit

---
+++ src/main/java/com/madhem/product_project/SpringStarterProjectApplication.java
@@ -0,0 +1,13 @@
+package com.madhem.product_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStarterProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringStarterProjectApplication.class, args);
+    }
+
+}
