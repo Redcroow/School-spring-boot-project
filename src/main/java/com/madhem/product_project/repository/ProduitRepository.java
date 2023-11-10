2a9bc5335cc608c5e3d7cc80a5d0f72ca06acb86
Redcroow <kilian.mvrl@gmail.com>
2023-11-10 15:38:58 +0100

    feat(dev): creat method query for findByCateg

--- src/main/java/com/madhem/product_project/repository/ProduitRepository.java
+++ src/main/java/com/madhem/product_project/repository/ProduitRepository.java
@@ -1,5 +1,6 @@
 package com.madhem.product_project.repository;
 
+import com.madhem.product_project.entities.Categorie;
 import com.madhem.product_project.entities.Produit;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -13,4 +14,7 @@ public interface ProduitRepository extends JpaRepository<Produit, Long> {
 
     @Query("select p from Produit p where p.nomProduit like concat('%', :nomSearch, '%') and p.prixProduit > :prixMinimum")
     List<Produit> findByNomContainNPrix (@Param("nomSearch") String nom, @Param("prixMinimum") Double prix);
+
+    @Query("select p from Produit p where p.categorie = ?1")
+    List<Produit> findByCategorie (Categorie categorie);
 }
