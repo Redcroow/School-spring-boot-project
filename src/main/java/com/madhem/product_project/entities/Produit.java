d23f27ef5230fd4af226516190b09dd9fa181045
Redcroow <kilian.mvrl@gmail.com>
2023-11-10 14:50:16 +0100

    feat(dev): add Categorie entitie for in Product entitie + getter and setter

--- src/main/java/com/madhem/product_project/entities/Produit.java
+++ src/main/java/com/madhem/product_project/entities/Produit.java
@@ -1,10 +1,7 @@
 package com.madhem.product_project.entities;
 
 import java.util.Date;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
 public class Produit {
@@ -14,6 +11,9 @@ public class Produit {
     private String nomProduit;
     private Double prixProduit;
     private Date dateCreation;
+
+    @ManyToOne
+    private Categorie categorie;
 
     public Produit() {
         super();
@@ -49,6 +49,12 @@ public class Produit {
     public void setDateCreation(Date dateCreation) {
         this.dateCreation = dateCreation;
     }
+    public Categorie getCategorie() {
+        return categorie;
+    }
+    public void setCategorie(Categorie categorie) {
+        this.categorie = categorie;
+    }
     @Override
     public String toString() {
         return "Produit [idProduit=" + idProduit + ", nomProduit=" +
@@ -56,6 +62,5 @@ public class Produit {
                 nomProduit + ", prixProduit=" + prixProduit
 
                 + ", dateCreation=" + dateCreation + "]";
-
     }
 }
