d3c48b7d598117a7e72d11a6630bcc602dfdbe7e
Redcroow <kilian.mvrl@gmail.com>
2023-11-10 15:15:18 +0100

    feat(dev): create method in repository with Query

--- src/main/java/com/madhem/product_project/repository/ProduitRepository.java
+++ src/main/java/com/madhem/product_project/repository/ProduitRepository.java
@@ -2,9 +2,15 @@ package com.madhem.product_project.repository;
 
 import com.madhem.product_project.entities.Produit;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import java.util.List;
 
 public interface ProduitRepository extends JpaRepository<Produit, Long> {
     List<Produit> findByNomProduit(String nom);
     List<Produit> findByNomProduitContains(String nom);
+
+    @Query("select p from Produit p where p.nomProduit like %:nom and p.prixProduit > :prix")
+    List<Produit> findByNomPrix (@Param("nomSearch") String nom, @Param("prixMinimum") Double prix);
 }
